291c1296058aa5a8620b1d1adb154f851f635cb8
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-09-19 15:33:27 -0700

    add image of puppy

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,28 @@ module.exports = {
           'css-loader',
           'sass-loader'
         ]
+      },
+
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.html$/,
+        use: [
+          'html-loader'
+        ]
       }
+
     ]
   },
 
